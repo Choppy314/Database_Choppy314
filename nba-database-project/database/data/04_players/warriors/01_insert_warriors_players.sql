0a82bd897a10dff9a4969fb6e408a41a30a337bd
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 14:00:20 +0600

    Add Warriors players

---
+++ nba-database-project/database/data/04_players/warriors/01_insert_warriors_players.sql
@@ -0,0 +1,18 @@
+-- Insert Golden State Warriors players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Stephen', 'Curry', 23, 15, '6-2', 185, 'Right'),
+('Jimmy', 'Butler', 23, 13, '6-6', 230, 'Right'),
+('Draymond', 'Green', 23, 12, '6-6', 230, 'Right'),
+('Gary', 'Payton II', 23, 8, '6-2', 195, 'Left'),
+('Buddy', 'Hield', 23, 8, '6-4', 220, 'Right'),
+('Kevon', 'Looney', 23, 9, '6-9', 222, 'Right'),
+('Jonathan', 'Kuminga', 23, 3, '6-7', 225, 'Right'),
+('Moses', 'Moody', 23, 3, '6-5', 211, 'Right'),
+('Brandin', 'Podziemski', 23, 1, '6-4', 205, 'Left'),
+('Gui', 'Santos', 23, 1, '6-7', 185, 'Right'),
+('Trayce', 'Jackson-Davis', 23, 1, '6-9', 245, 'Left'),
+('Quinten', 'Post', 23, 0, '7-0', 238, 'Right'),
+('Pat', 'Spencer', 23, 1, '6-2', 205, 'Right'),
+('Kevin', 'Knox', 23, 6, '6-8', 215, 'Right'),
+('Yuri', 'Collins', 23, 0, '6-0', 190, 'Right');
