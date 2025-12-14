0855d98a9f40ec1e211ee78527240bf70f812691
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 18:32:19 +0600

    Add Pelicans players

---
+++ nba-database-project/database/data/04_players/pelicans/01_insert_pelicans_players.sql
@@ -0,0 +1,18 @@
+-- Insert New Orleans Pelicans players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Zion', 'Williamson', 27, 4, '6-6', 284, 'Left'),
+('CJ', 'McCollum', 27, 11, '6-3', 190, 'Right'), 
+('Dejounte', 'Murray', 27, 7, '6-4', 180, 'Right'), 
+('Bruce', 'Brown', 27, 6, '6-4', 202, 'Right'), 
+('Herbert', 'Jones', 27, 3, '6-7', 206, 'Left'), 
+('Kelly', 'Olynyk', 27, 11, '7-0', 240, 'Right'),
+('Trey', 'Murphy III', 27, 3, '6-8', 206, 'Right'),
+('Jordan', 'Hawkins', 27, 1, '6-5', 190, 'Right'),
+('Yves', 'Missi', 27, 0, '6-11', 235, 'Right'),
+('Jeremiah', 'Robinson-Earl', 27, 3, '6-9', 240, 'Right'),
+('Jose', 'Alvarado', 27, 3, '6-0', 179, 'Right'),
+('Javonte', 'Green', 27, 5, '6-5', 205, 'Right'),
+('Karlo', 'Matković', 27, 0, '6-10', 231, 'Right'),
+('Antonio', 'Reeves', 27, 0, '6-5', 205, 'Right'),
+('Brandon', 'Boston Jr.', 27, 3, '6-6', 188, 'Right');
