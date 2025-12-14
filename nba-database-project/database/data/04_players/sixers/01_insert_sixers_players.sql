62e296394f909dd5af94bb33c9fdf0e051841236
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 19:59:14 +0600

    Add 76ers players

---
+++ nba-database-project/database/data/04_players/sixers/01_insert_sixers_players.sql
@@ -0,0 +1,18 @@
+-- Insert Philadelphia 76ers players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Joel', 'Embiid', 3, 8, '7-0', 280, 'Right'),
+('Paul', 'George', 3, 14, '6-8', 220, 'Right'),
+('Tyrese', 'Maxey', 3, 4, '6-2', 200, 'Right'),
+('Kelly', 'Oubre Jr.', 3, 9, '6-8', 203, 'Left'),
+('Andre', 'Drummond', 3, 12, '6-11', 279, 'Right'),
+('Quentin', 'Grimes', 3, 3, '6-4', 210, 'Right'),
+('Jared', 'McCain', 3, 0, '6-3', 195, 'Right'),
+('Eric', 'Gordon', 3, 16, '6-3', 215, 'Right'),
+('Kyle', 'Lowry', 3, 18, '6-0', 196, 'Right'),
+('Guerschon', 'Yabusele', 3, 2, '6-7', 265, 'Right'),
+('Ricky', 'Council IV', 3, 1, '6-6', 207, 'Right'),
+('Adem', 'Bona', 3, 0, '6-10', 235, 'Right'),
+('Lonnie', 'Walker IV', 3, 6, '6-4', 204, 'Right'),
+('Jared', 'Butler', 3, 3, '6-3', 193, 'Right'),
+('Justin', 'Edwards', 3, 0, '6-7', 203, 'Left');
