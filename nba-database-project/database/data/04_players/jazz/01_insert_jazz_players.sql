3a47910c8b0cce8081bdb8564844bc366a703718
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 16:46:05 +0600

    Add Jazz players

---
+++ nba-database-project/database/data/04_players/jazz/01_insert_jazz_players.sql
@@ -0,0 +1,18 @@
+-- Insert Utah Jazz players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Lauri', 'Markkanen', 19, 7, '7-1', 240, 'Right'),
+('John', 'Collins', 19, 7, '6-9', 226, 'Right'),
+('Collin', 'Sexton', 19, 6, '6-3', 190, 'Right'),
+('Jordan', 'Clarkson', 19, 10, '6-5', 194, 'Right'),
+('KJ', 'Martin', 19, 4, '6-6', 215, 'Right'),
+('Taylor', 'Hendricks', 19, 1, '6-9', 215, 'Right'),
+('Cody', 'Williams', 19, 0, '6-8', 190, 'Right'),
+('Keyonte', 'George', 19, 1, '6-4', 185, 'Right'),
+('Svi', 'Mykhailiuk', 19, 6, '6-7', 205, 'Right'),
+('Johnny', 'Juzang', 19, 2, '6-7', 226, 'Right'),
+('Kyle', 'Filipowski', 19, 0, '6-11', 250, 'Right'),
+('Walker', 'Kessler', 19, 2, '7-2', 245, 'Right'),
+('Brice', 'Sensabaugh', 19, 1, '6-6', 235, 'Right'),
+('Isaiah', 'Collier', 19, 0, '6-4', 210, 'Right'),
+('Oscar', 'Tshiebwe', 19, 1, '6-8', 255, 'Right');
