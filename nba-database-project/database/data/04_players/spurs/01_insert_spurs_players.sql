3daf0ada441d65858abb0fdd47c1ef591fa82339
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 18:02:14 +0600

    Add Spurs players

---
+++ nba-database-project/database/data/04_players/spurs/01_insert_spurs_players.sql
@@ -0,0 +1,18 @@
+-- Insert San Antonio Spurs players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('De`Aaron', 'Fox', 30, 7, '6-3', 185, 'Left'),
+('Devin', 'Vassell', 30, 4, '6-5', 200, 'Right'),
+('Keldon', 'Johnson', 30, 5, '6-6', 220, 'Right'),
+('Harrison', 'Barnes', 30, 12, '6-7', 225, 'Right'),
+('Victor', 'Wembanyama', 30, 1, '7-4', 235, 'Right'),
+('Chris', 'Paul', 30, 19, '6-0', 175, 'Right'),
+('Stephon', 'Castle', 30, 0, '6-6', 215, 'Right'),
+('Jeremy', 'Sochan', 30, 2, '6-8', 230, 'Right'),
+('Malaki', 'Branham', 30, 2, '6-4', 180, 'Right'),
+('Julian', 'Champagnie', 30, 2, '6-7', 217, 'Right'),
+('Blake', 'Wesley', 30, 2, '6-4', 190, 'Right'),
+('Jordan', 'McLaughlin', 30, 5, '5-11', 185, 'Right'),
+('Charles', 'Bassey', 30, 3, '6-10', 230, 'Right'),
+('Sandro', 'Mamukelashvili', 30, 3, '6-9', 240, 'Left'),
+('Bismack', 'Biyombo', 30, 13, '6-8', 255, 'Right');
