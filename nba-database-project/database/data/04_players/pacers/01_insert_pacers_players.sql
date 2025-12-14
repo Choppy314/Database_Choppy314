60757575412727766c08067b33b45042b83dd580
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 20:30:28 +0600

    Add Pacers players

---
+++ nba-database-project/database/data/04_players/pacers/01_insert_pacers_players.sql
@@ -0,0 +1,17 @@
+-- Insert Indiana Pacers players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Pascal', 'Siakam', 8, 8, '6-8', 245, 'Right'),
+('Tyrese', 'Haliburton', 8, 4, '6-5', 185, 'Right'),
+('Myles', 'Turner', 8, 9, '6-11', 250, 'Right'),
+('Obi', 'Toppin', 8, 4, '6-9', 220, 'Right'),
+('Aaron', 'Nesmith', 8, 4, '6-5', 215, 'Right'),
+('T.J.', 'McConnell', 8, 9, '6-1', 190, 'Right'),
+('Bennedict', 'Mathurin', 8, 2, '6-5', 210, 'Right'),
+('Jarace', 'Walker', 8, 1, '6-7', 235, 'Right'),
+('Isaiah', 'Jackson', 8, 3, '6-8', 205, 'Right'),
+('Ben', 'Sheppard', 8, 1, '6-6', 190, 'Right'),
+('Thomas', 'Bryant', 8, 7, '6-9', 248, 'Right'),
+('James', 'Johnson', 8, 15, '6-7', 240, 'Right'),
+('Andrew', 'Nembhard', 8, 2, '6-4', 191, 'Right'),
+('Johnny', 'Furphy', 8, 0, '6-8', 200, 'Right');
