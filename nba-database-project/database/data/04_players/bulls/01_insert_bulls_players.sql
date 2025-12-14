f3859183e04ff87eeb5aed67f67d4c523dc224af
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 21:24:34 +0600

    Add Bulls players

---
+++ nba-database-project/database/data/04_players/bulls/01_insert_bulls_players.sql
@@ -0,0 +1,17 @@
+-- Insert Chicago Bulls players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Lonzo', 'Ball', 9, 5, '6-5', 190, 'Right'),
+('Nikola', 'Vučević', 9, 13, '6-9', 260, 'Right'),
+('Patrick', 'Williams', 9, 4, '6-6', 215, 'Right'),
+('Kevin', 'Huerter', 9, 6, '6-6', 198, 'Right'),
+('Zach', 'Collins', 9, 6, '6-9', 250, 'Right'),
+('Coby', 'White', 9, 5, '6-4', 195, 'Right'),
+('Tre', 'Jones', 9, 4, '6-1', 185, 'Right'),
+('Jalen', 'Smith', 9, 4, '6-8', 215, 'Right'),
+('Josh', 'Giddey', 9, 3, '6-7', 216, 'Right'),
+('Ayo', 'Dosunmu', 9, 3, '6-4', 200, 'Right'),
+('Jevon', 'Carter', 9, 6, '6-0', 200, 'Right'),
+('Matas', 'Buzelis', 9, 0, '6-8', 209, 'Right'),
+('Dalen', 'Terry', 9, 2, '6-6', 195, 'Right'),
+('Julian', 'Phillips', 9, 1, '6-6', 198, 'Right');
