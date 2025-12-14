c63bff8a9d4fd61a75c75d4aca09f8aaf4d7cda4
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 19:25:06 +0600

    Add Raptors players

---
+++ nba-database-project/database/data/04_players/raptors/01_insert_raptors_players.sql
@@ -0,0 +1,17 @@
+-- Insert Toronto Raptors players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Brandon', 'Ingram', 5, 8, '6-8', 190, 'Right'),
+('Immanuel', 'Quickley', 5, 4, '6-2', 190, 'Right'),
+('RJ', 'Barrett', 5, 5, '6-6', 214, 'Left'),
+('Jakob', 'Poeltl', 5, 8, '7-0', 253, 'Right'),
+('Chris', 'Boucher', 5, 7, '6-8', 200, 'Right'),
+('Scottie', 'Barnes', 5, 3, '6-8', 237, 'Right'),
+('Gradey', 'Dick', 5, 1, '6-7', 200, 'Right'),
+('Ochai', 'Agbaji', 5, 2, '6-5', 215, 'Right'),
+('Ja`Kobe', 'Walter', 5, 0, '6-4', 180, 'Right'),
+('Garrett', 'Temple', 5, 14, '6-5', 195, 'Right'),
+('Jamal', 'Shead', 5, 0, '6-1', 200, 'Right'),
+('Jonathan', 'Mogbo', 5, 0, '6-9', 225, 'Right'),
+('Bruno', 'Fernando', 5, 5, '6-9', 240, 'Right'),
+('Jamison', 'Battle', 5, 0, '6-7', 220, 'Left');
