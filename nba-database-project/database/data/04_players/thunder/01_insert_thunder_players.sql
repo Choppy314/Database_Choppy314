06e2fda7ee4058b08b8e78002996b7440420807c
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 15:13:06 +0600

    Add Thunder players

---
+++ nba-database-project/database/data/04_players/thunder/01_insert_thunder_players.sql
@@ -0,0 +1,18 @@
+-- Insert Oklahoma City Thunder players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Shai', 'Gilgeous-Alexander', 16, 6, '6-6', 195, 'Right'),
+('Isaiah', 'Hartenstein', 16, 6, '7-0', 250, 'Left'),
+('Luguentz', 'Dort', 16, 5, '6-4', 220, 'Right'),
+('Isaiah', 'Joe', 16, 4, '6-4', 165, 'Right'),
+('Chet', 'Holmgren', 16, 1, '7-1', 208, 'Right'),
+('Aaron', 'Wiggins', 16, 3, '6-5', 190, 'Right'),
+('Alex', 'Caruso', 16, 7, '6-5', 186, 'Right'),
+('Kenrich', 'Williams', 16, 6, '6-7', 210, 'Right'),
+('Cason', 'Wallace', 16, 1, '6-3', 195, 'Right'),
+('Ousmane', 'Dieng', 16, 2, '6-9', 185, 'Right'),
+('Jalen', 'Williams', 16, 2, '6-5', 211, 'Right'),
+('Ajay', 'Mitchell', 16, 0, '6-4', 190, 'Left'),
+('Dillon ', 'Jones', 16, 0, '6-5', 235, 'Right'),
+('Jaylin', 'Williams', 16, 2, '6-9', 240, 'Right'),
+('Branden', 'Carlson', 16, 0, '7-0', 220, 'Right');
