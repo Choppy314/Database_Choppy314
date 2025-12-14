3f943ce9a1e1442944125897096a5d2d8e837844
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 12:46:58 +0600

    Add Lakers players and remove salaries

---
+++ nba-database-project/database/data/04_insert_lakers_players.sql
@@ -0,0 +1,18 @@
+-- Insert Los Angeles Lakers players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('LeBron', 'James', 21, 21, '6-9', 250, 'Right'),
+('Luka', 'Dončić', 21, 6, '6-8', 230, 'Right'),
+('Austin', 'Reaves', 21, 3, '6-5', 197, 'Right'),
+('Dorian', 'Finney-Smith', 21, 7, '6-7', 220, 'Right'),
+('Rui', 'Hachimura', 21, 5, '6-8', 230, 'Right'),
+('Gabe', 'Vincent', 21, 5, '6-2', 200, 'Right'),
+('Jarred', 'Vanderbilt', 21, 6, '6-8', 214, 'Right'),
+('Jaxson', 'Hayes', 21, 5, '7-0', 220, 'Right'),
+('Dalton', 'Knecht', 21, 0, '6-6', 215, 'Right'),
+('Cam', 'Reddish', 21, 5, '6-7', 230, 'Right'),
+('Christian', 'Wood', 21, 7, '6-8', 214, 'Right'),
+('Maxi', 'Kleber', 21, 7, '6-10', 240, 'Right'),
+('Bronny', 'James', 21, 0, '6-2', 210, 'Right'),
+('Shake', 'Milton', 21, 6, '6-5', 205, 'Right'),
+('Markieff', 'Morris', 21, 13, '6-9', 245, 'Right');
