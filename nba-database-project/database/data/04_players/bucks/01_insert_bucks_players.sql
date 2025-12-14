172e723d87e4364cd9162a75a48ee4775a31aea6
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 20:45:37 +0600

    Add Bucks players

---
+++ nba-database-project/database/data/04_players/bucks/01_insert_bucks_players.sql
@@ -0,0 +1,17 @@
+-- Insert Milwaukee Bucks players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Giannis', 'Antetokounmpo', 6, 11, '6-11', 243, 'Right'),
+('Damian', 'Lillard', 6, 12, '6-2', 200, 'Right'),
+('Kyle', 'Kuzma', 6, 7, '6-8', 221, 'Right'),
+('Brook', 'Lopez', 6, 16, '7-1', 282, 'Right'),
+('Bobby', 'Portis', 6, 9, '6-9', 250, 'Right'),
+('Pat', 'Connaughton', 6, 9, '6-5', 209, 'Right'),
+('Kevin', 'Porter Jr.', 6, 4, '6-5', 203, 'Left'),
+('A.J.', 'Green', 6, 2, '6-4', 190, 'Right'),
+('Jericho', 'Sims', 6, 3, '6-10', 250, 'Right'),
+('Taurean', 'Prince', 6, 8, '6-6', 218, 'Right'),
+('Gary', 'Trent Jr.', 6, 6, '6-5', 204, 'Right'),
+('Andre', 'Jackson Jr.', 6, 1, '6-6', 209, 'Right'),
+('Chris', 'Livingston', 6, 1, '6-6', 220, 'Right'),
+('Ryan', 'Rollins', 6, 2, '6-3', 180, 'Right');
