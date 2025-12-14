93a705a3f586eca8911b5f0a27621201399cf349
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 14:23:32 +0600

    Add Kings players

---
+++ nba-database-project/database/data/04_players/kings/01_insert_kings_players.sql
@@ -0,0 +1,18 @@
+-- Insert Sacramento Kings players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Zach', 'Lavine', 25, 10, '6-5', 200, 'Right'),
+('Domantas', 'Sabonis', 25, 8, '6-10', 240, 'Left'),
+('DeMar', 'DeRozan', 25, 15, '6-6', 210, 'Right'),
+('Malik', 'Monk', 25, 7, '6-3', 200, 'Right'),
+('Jonas', 'Valančiūnas', 25, 12, '6-11', 265, 'Right'),
+('Keegan', 'Murray', 25, 2, '6-8', 225, 'Right'),
+('Trey', 'Lyles', 25, 9, '6-9', 234, 'Right'),
+('Devin', 'Carter', 25, 0, '6-2', 195, 'Right'),
+('Jake', 'LaRavia', 25, 2, '6-7', 235, 'Right'),
+('Keon', 'Ellis', 25, 2, '6-4', 175, 'Right'),
+('Doug', 'McDermott', 25, 10, '6-7', 225, 'Right'),
+('Jae', 'Crowder', 25, 12, '6-6', 235, 'Right'),
+('Orlando', 'Robinson', 25, 2, '6-10', 235, 'Right'),
+('Markelle', 'Fultz', 25, 7, '6-4', 209, 'Right'),
+('Isaac', 'Jones', 25, 0, '6-8', 245, 'Right');
