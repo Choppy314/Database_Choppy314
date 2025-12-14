72d173952fb720cd58d6794cec0f08e3068a351b
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 17:24:50 +0600

    Add Grizzlies players

---
+++ nba-database-project/database/data/04_players/grizzlies/01_insert_grizzlies_players.sql
@@ -0,0 +1,18 @@
+-- Insert Memphis Grizzlies players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Ja', 'Morant', 29, 5, '6-2', 174, 'Right'),
+('Desmond', 'Bane', 29, 4, '6-6', 215, 'Right'),
+('Jaren', 'Jackson Jr.', 29, 6, '6-10', 242, 'Right'),
+('Marvin', 'Bagley III', 29, 6, '6-10', 235, 'Left'),
+('Brandon', 'Clarke', 29, 5, '6-8', 215, 'Right'),
+('Luke', 'Kennard', 29, 7, '6-5', 206, 'Left'),
+('John', 'Konchar', 29, 5, '6-5', 210, 'Right'),
+('Zach', 'Edey', 29, 0, '7-3', 305, 'Right'),
+('Santi', 'Aldama', 29, 3, '7-0', 215, 'Right'),
+('Vince', 'Williams Jr.', 29, 2, '6-4', 205, 'Left'),
+('Jay', 'Huff', 29, 3, '7-1', 240, 'Right'),
+('Scotty', 'Pippen Jr.', 29, 2, '6-2', 170, 'Right'),
+('GG', 'Jackson II', 29, 1, '6-9', 210, 'Right'),
+('Jaylen', 'Wells', 29, 0, '6-7', 206, 'Right'),
+('Cam', 'Spencer', 29, 0, '6-6', 205, 'Right');
