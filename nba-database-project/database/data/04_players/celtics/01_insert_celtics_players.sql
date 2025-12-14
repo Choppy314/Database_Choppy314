9956a21c53ff792dd0c56a0cbb0faf4c72bd33bb
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 18:49:08 +0600

    Add Celtics players

---
+++ nba-database-project/database/data/04_players/celtics/01_insert_celtics_players.sql
@@ -0,0 +1,18 @@
+-- Insert Boston Celtics players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Jaylen', 'Brown', 1, 8, '6-6', 223, 'Right'),
+('Jayson', 'Tatum', 1, 7, '6-8', 210, 'Right'),
+('Jrue', 'Holiday', 1, 15, '6-4', 220, 'Right'),
+('Kristaps', 'Porziņģis', 1, 8, '7-2', 240, 'Right'),
+('Derrick', 'White', 1, 7, '6-4', 190, 'Right'),
+('Al', 'Horford', 1, 17, '6-8', 240, 'Right'),
+('Payton', 'Pritchard', 1, 4, '6-1', 195, 'Right'),
+('Baylor', 'Scheierman', 1, 0, '6-6', 205, 'Left'),
+('Xavier', 'Tillman Sr.', 1, 4, '6-8', 245, 'Right'),
+('Neemias', 'Queta', 1, 3, '7-0', 248, 'Right'),
+('Sam', 'Hauser', 1, 3, '6-7', 217, 'Right'),
+('Luke', 'Kornet', 1, 7, '7-1', 250, 'Right'),
+('Jordan', 'Walsh', 1, 1, '6-6', 205, 'Right'),
+('Torrey', 'Craig', 1, 7, '6-5', 221, 'Right'),
+('JD', 'Davison', 1, 2, '6-1', 195, 'Right');
