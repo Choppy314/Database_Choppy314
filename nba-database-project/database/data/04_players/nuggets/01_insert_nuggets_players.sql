766131bd0c3eab6f3b758d3b577054112926b145
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 15:34:55 +0600

    Add Nuggets players

---
+++ nba-database-project/database/data/04_players/nuggets/01_insert_nuggets_players.sql
@@ -0,0 +1,18 @@
+-- Insert Denver Nuggets players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Nikola', 'Jokić', 17, 9, '6-11', 284, 'Right'),
+('Jamal', 'Murray', 17, 7, '6-4', 215, 'Right'),
+('Michael', 'Porter Jr.', 17, 5, '6-10', 218, 'Right'),
+('Aaron', 'Gordon', 17, 10, '6-8', 235, 'Right'),
+('Zeke', 'Nnaji', 17, 5, '6-10', 240, 'Right'),
+('Dario', 'Šarić', 17, 7, '6-10', 225, 'Right'),
+('Russell', 'Westbrook', 17, 16, '6-4', 200, 'Right'),
+('Christian', 'Braun', 17, 2, '6-6', 220, 'Right'),
+('DaRon', 'Holmes', 17, 0, '6-9', 225, 'Right'),
+('Julian', 'Strawther', 17, 1, '6-6', 205, 'Right'),
+('Peyton', 'Watson', 17, 2, '6-8', 200, 'Right'),
+('Vlatko', 'Čančar', 17, 4, '6-8', 236, 'Right'),
+('DeAndre', 'Jordan', 17, 16, '6-11', 265, 'Left'),
+('Hunter', 'Tyson', 17, 1, '6-8', 215, 'Right'),
+('Jalen', 'Pickett', 17, 1, '6-2', 202, 'Right');
