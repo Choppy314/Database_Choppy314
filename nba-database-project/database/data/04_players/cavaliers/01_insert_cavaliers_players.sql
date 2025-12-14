8fcafa0360dc80f032439a9cb82f1389d82aaf37
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 20:15:02 +0600

    Add Cavaliers players

---
+++ nba-database-project/database/data/04_players/cavaliers/01_insert_cavaliers_players.sql
@@ -0,0 +1,16 @@
+-- Insert Cleveland Cavaliers players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Darius', 'Garland', 7, 5, '6-1', 192, 'Right'),
+('Donovan', 'Mitchell', 7, 7, '6-2', 215, 'Right'),
+('De`Andre', 'Hunter', 7, 5, '6-7', 221, 'Right'),
+('Jarrett', 'Allen', 7, 7, '6-9', 243, 'Right'),
+('Max', 'Strus', 7, 4, '6-5', 215, 'Right'),
+('Evan', 'Mobley', 7, 3, '6-11', 215, 'Right'),
+('Isaac', 'Okoro', 7, 4, '6-4', 225, 'Right'),
+('Dean', 'Wade', 7, 5, '6-9', 228, 'Right'),
+('Jaylon', 'Tyson', 7, 0, '6-6', 215, 'Right'),
+('Ty', 'Jerome', 7, 5, '6-5', 195, 'Right'),
+('Sam', 'Merrill', 7, 4, '6-4', 205, 'Right'),
+('Tristan', 'Thompson', 7, 12, '6-9', 254, 'Right'),
+('Craig', 'Porter Jr.', 7, 1, '6-1', 180, 'Right');
