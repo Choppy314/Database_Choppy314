172b459f59988bb0e869293ce252250022b81789
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 21:59:04 +0600

    Add Hawks players

---
+++ nba-database-project/database/data/04_players/hawks/01_insert_hawks_players.sql
@@ -0,0 +1,16 @@
+-- Insert Atlanta Hawks players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Trae', 'Young', 12, 6, '6-2', 164, 'Right'),
+('Clint', 'Capela', 12, 10, '6-10', 256, 'Right'),
+('Caris', 'LeVert', 12, 8, '6-7', 205, 'Right'),
+('Onyeka', 'Okongwu', 12, 4, '6-10', 240, 'Right'),
+('Zaccharie', 'Risacher', 12, 0, '6-8', 200, 'Right'),
+('Terance', 'Mann', 12, 5, '6-6', 215, 'Right'),
+('Larry', 'Nance Jr.', 12, 9, '6-6', 245, 'Right'),
+('Dyson', 'Daniels', 12, 2, '6-7', 199, 'Right'),
+('Jalen', 'Johnson', 12, 3, '6-8', 219, 'Right'),
+('Kobe', 'Bufkin', 12, 1, '6-8', 219, 'Left'),
+('Garrison', 'Mathews', 12, 5, '6-5', 215, 'Right'),
+('Vit', 'Krejci', 12, 3, '6-8', 195, 'Right'),
+('Mouhamed', 'Gueye', 12, 1, '6-11', 210, 'Right');
