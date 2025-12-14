5740d1117616538c57c4b728cc4581c6e9d8fec6
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 19:07:22 +0600

    Add Knicks players

---
+++ nba-database-project/database/data/04_players/knicks/01_insert_knicks_players.sql
@@ -0,0 +1,18 @@
+-- Insert New York Knicks players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Karl-Anthony', 'Towns', 2, 9, '7-0', 248, 'Right'),
+('OG', 'Anunoby', 2, 7, '6-7', 240, 'Right'),
+('Jalen', 'Brunson', 2, 6, '6-2', 190, 'Left'),
+('Mikal', 'Bridges', 2, 6, '6-6', 209, 'Right'),
+('Josh', 'Hart', 2, 7, '6-5', 215, 'Right'),
+('Mitchell', 'Robinson', 2, 6, '7-0', 240, 'Right'),
+('Precious', 'Achiuwa', 2, 4, '6-8', 243, 'Right'),
+('Miles', 'McBride', 2, 3, '6-2', 195, 'Right'),
+('Delon', 'Wright', 2, 9, '6-4', 185, 'Right'),
+('Tyler', 'Kolek', 2, 0, '6-2', 195, 'Left'),
+('Cameron', 'Payne', 2, 9, '6-2', 183, 'Left'),
+('Pacome', 'Dadiet', 2, 0, '6-9', 210, 'Right'),
+('Landry', 'Shamet', 2, 6, '6-5', 190, 'Right'),
+('Ariel', 'Hukporti', 2, 0, '7-0', 246, 'Left'),
+('Matt', 'Ryan', 2, 3, '6-6', 215, 'Right');
