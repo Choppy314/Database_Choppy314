667bcf1e1d837c85ab80a7a88deaac7f43cd353e
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 19:41:49 +0600

    Add Nets players

---
+++ nba-database-project/database/data/04_players/nets/01_insert_nets_players.sql
@@ -0,0 +1,16 @@
+-- Insert Brooklyn Nets players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Nic', 'Claxton', 4, 5, '6-11', 215, 'Left'),
+('Cameron', 'Johnson', 4, 5, '6-8', 210, 'Right'),
+('D`Angelo', 'Russell', 4, 9, '6-3', 193, 'Left'),
+('Ziaire', 'Williams', 4, 3, '6-9', 185, 'Right'),
+('Cam', 'Thomas', 4, 3, '6-3', 210, 'Right'),
+('Day`Ron', 'Sharpe', 4, 3, '6-10', 265, 'Right'),
+('Noah', 'Clowney', 4, 1, '6-10', 210, 'Right'),
+('Dariq', 'Whitehead', 4, 1, '6-6', 220, 'Right'),
+('Trendon', 'Watford', 4, 3, '6-8', 237, 'Right'),
+('Keon', 'Johnson', 4, 3, '6-5', 185, 'Right'),
+('Maxwell', 'Lewis', 4, 1, '6-7', 215, 'Right'),
+('Jalen', 'Wilson', 4, 1, '6-6', 220, 'Right'),
+('Tyrese', 'Martin', 4, 1, '6-6', 215, 'Right');
