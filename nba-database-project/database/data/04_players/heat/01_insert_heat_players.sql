c3c194c6dafbe4603bf0675f96b3da0a7a8d0ebf
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 22:15:25 +0600

    Add Heat players

---
+++ nba-database-project/database/data/04_players/heat/01_insert_heat_players.sql
@@ -0,0 +1,18 @@
+-- Insert Miami Heat players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Bam', 'Adebayo', 11, 7, '6-9', 255, 'Right'),
+('Tyler', 'Herro', 11, 5, '6-5', 195, 'Right'),
+('Andrew', 'Wiggins', 11, 10, '6-6', 197, 'Right'),
+('Terry', 'Rozier', 11, 8, '6-1', 190, 'Right'),
+('Duncan', 'Robinson', 11, 6, '6-7', 215, 'Right'),
+('Kyle', 'Anderson', 11, 10, '6-8', 230, 'Right'),
+('Davion', 'Mitchell', 11, 3, '6-0', 202, 'Right'),
+('Haywood', 'Highsmith', 11, 4, '6-5', 220, 'Right'),
+('Kel`el', 'Ware', 11, 0, '7-0', 230, 'Right'),
+('Kevin', 'Love', 11, 16, '6-10', 251, 'Right'),
+('Jaime', 'Jaquez Jr.', 11, 1, '6-6', 225, 'Right'),
+('Nikola', 'Jović', 11, 2, '6-10', 205, 'Right'),
+('Alec', 'Burks', 11, 13, '6-5', 214, 'Right'),
+('Pelle', 'Larsson', 11, 0, '6-5', 215, 'Right'),
+('Keshad', 'Johnson', 11, 0, '6-6', 225, 'Right');
