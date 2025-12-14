162fcd613d304fcec73f278ba5a469c5d93f3ec5
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 16:22:06 +0600

    Add Blazers players

---
+++ nba-database-project/database/data/04_players/blazers/01_insert_blazers_players.sql
@@ -0,0 +1,18 @@
+-- Insert Portland Trail Blazers players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Deandre', 'Ayton', 20, 6, '7-0', 252, 'Right'),
+('Jerami', 'Grant', 20, 10, '6-7', 213, 'Right'),
+('Anfernee', 'Simons', 20, 6, '6-3', 200, 'Right'),
+('Deni', 'Avdija', 20, 4, '6-8', 228, 'Right'),
+('Robert', 'Williams', 20, 6, '6-9', 249, 'Right'),
+('Matisse', 'Thybulle', 20, 5, '6-5', 202, 'Right'),
+('Scoot', 'Henderson', 20, 1, '6-3', 207, 'Right'),
+('Donovan', 'Clingan', 20, 0, '7-2', 280, 'Right'),
+('Shaedon', 'Sharpe', 20, 2, '6-5', 210, 'Right'),
+('Kris', 'Murray', 20, 1, '6-8', 218, 'Left'),
+('Dalano', 'Banton', 20, 3, '6-8', 203, 'Right'),
+('Duop', 'Reath', 20, 1, '6-9', 245, 'Right'),
+('Jabari', 'Walker', 20, 2, '6-7', 237, 'Right'),
+('Toumani', 'Camara', 20, 1, '6-7', 230, 'Left'),
+('Rayan', 'Rupert', 20, 1, '6-7', 205, 'Right');
