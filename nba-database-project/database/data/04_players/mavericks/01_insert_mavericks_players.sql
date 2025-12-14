7e9dd0cc1e36dc5fcdd4430961dc2e3f8b97cf7c
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 17:43:04 +0600

    Add Mavericks players

---
+++ nba-database-project/database/data/04_players/mavericks/01_insert_mavericks_players.sql
@@ -0,0 +1,18 @@
+-- Insert Dallas Mavericks players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Anthony', 'Davis', 26, 12, '6-10', 253, 'Right'),
+('Kyrie', 'Irving', 26, 13, '6-2', 195, 'Right'),
+('Klay', 'Thompson', 26, 11, '6-5', 220, 'Right'),
+('P.J.', 'Washington', 26, 15, '6-7', 230, 'Right'),
+('Daniel', 'Gafford', 26, 15, '6-10', 265, 'Right'),
+('Caleb', 'Martin', 26, 5, '6-5', 205, 'Right'),
+('Naji', 'Marshall', 26, 4, '6-6', 220, 'Right'),
+('Max ', 'Christie', 26, 2, '6-5', 190, 'Right'),
+('Dereck', 'Lively II', 26, 1, '7-1', 230, 'Right'),
+('Dwight', 'Powell', 26, 10, '6-10', 240, 'Right'),
+('Dante', 'Exum', 26, 7, '6-5', 214, 'Right'),
+('Olivier-Maxence', 'Prosper', 26, 1, '6-7', 230, 'Right'),
+('Spencer', 'Dinwiddie', 26, 10, '6-5', 215, 'Right'),
+('Jaden', 'Hardy', 26, 2, '6-3', 198, 'Right'),
+('Brandon', 'Williams', 26, 2, '6-1', 190, 'Right');
