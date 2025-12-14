007885a45993cd127726a07e6e1e0c17e7dcc0fd
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 21:08:22 +0600

    Add Pistons players

---
+++ nba-database-project/database/data/04_players/pistons/01_insert_pistons_players.sql
@@ -0,0 +1,17 @@
+-- Insert Detroit Pistons players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Tobias', 'Harris', 10, 13, '6-8', 226, 'Right'),
+('Tim', 'Hardaway Jr.', 10, 11, '6-5', 205, 'Right'),
+('Isaiah', 'Stewart', 10, 3, '6-8', 250, 'Right'),
+('Cade', 'Cunningham', 10, 3, '6-6', 220, 'Right'),
+('Dennis', 'Schröder', 10, 11, '6-1', 172, 'Right'),
+('Ausar', 'Thompson', 10, 1, '6-7', 215, 'Right'),
+('Ron', 'Holland', 10, 0, '6-8', 206, 'Left'),
+('Jaden', 'Ivey', 10, 2, '6-4', 195, 'Right'),
+('Simone', 'Fontecchio', 10, 2, '6-7', 209, 'Right'),
+('Malik', 'Beasley', 10, 8, '6-4', 187, 'Right'),
+('Jalen', 'Duren', 10, 2, '6-10', 250, 'Right'),
+('Marcus', 'Sasser', 10, 1, '6-1', 195, 'Right'),
+('Paul', 'Reed', 10, 4, '6-9', 210, 'Right'),
+('Lindy', 'Waters III', 10, 3, '6-6', 210, 'Right');
