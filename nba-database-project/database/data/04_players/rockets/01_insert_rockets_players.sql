7c94223ff656f36d62d68e96f8f3140488386729
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 17:06:20 +0600

    Add Rockets players

---
+++ nba-database-project/database/data/04_players/rockets/01_insert_rockets_players.sql
@@ -0,0 +1,18 @@
+-- Insert Houston Rockets players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Fred', 'VanVleet', 28, 8, '6-0', 197, 'Right'),
+('Dillon', 'Brooks', 28, 7, '6-7', 225, 'Right'),
+('Steven', 'Adams', 28, 10, '6-11', 265, 'Right'),
+('Jalen', 'Green', 28, 3, '6-4', 186, 'Right'),
+('Reed', 'Shppard', 28, 0, '6-2', 185, 'Right'),
+('Jabari', 'Smith Jr.', 28, 2, '6-11', 220, 'Right'),
+('Amen', 'Thompson', 28, 1, '6-7', 200, 'Right'),
+('Jeff', 'Green', 28, 16, '6-8', 235, 'Right'),
+('Jock', 'Landale', 28, 3, '6-11', 255, 'Right'),
+('Jae`Sean', 'Tate', 28, 4, '6-4', 230, 'Left'),
+('Alperen ', 'Şengün', 28, 3, '6-11', 243, 'Right'),
+('Aaron ', 'Holiday', 28, 6, '6-0', 185, 'Right'),
+('Tari', 'Eason', 28, 2, '6-8', 215, 'Right'),
+('Cam', 'Whitmore', 28, 1, '6-6', 230, 'Right'),
+('Jeenathan', 'Williams', 28, 2, '6-5', 205, 'Left');
