e035c0bb1dc97bf477d2756bc8eefca86d8b247f
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 14:50:36 +0600

    Add Suns players

---
+++ nba-database-project/database/data/04_players/suns/01_insert_suns_players.sql
@@ -0,0 +1,18 @@
+-- Insert Phoenix Suns players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Kevin', 'Durant', 22, 16, '6-11', 240, 'Right'),
+('Bradley', 'Beal', 22, 12, '6-4', 207, 'Right'),
+('Devin', 'Booker', 22, 9, '6-5', 206, 'Right'),
+('Grayson', 'Allen', 22, 6, '6-3', 198, 'Right'),
+('Royce', 'O`Neale', 22, 7, '6-6', 226, 'Right'),
+('Cody', 'Martin', 22, 5, '6-6', 205, 'Right'),
+('Vasilije', 'Micić', 22, 1, '6-3', 188, 'Right'),
+('Nick', 'Richards', 22, 4, '6-11', 245, 'Right'),
+('Ryan', 'Dunn', 22, 0, '6-7', 216, 'Right'),
+('Damion', 'Lee', 22, 6, '6-6', 210, 'Right'),
+('Tyus', 'Jones', 22, 9, '6-0', 196, 'Right'),
+('Mason', 'Plumlee', 22, 11, '7-0', 254, 'Left'),
+('Monte', 'Morris', 22, 7, '6-2', 183, 'Right'),
+('Bol', 'Bol', 22, 5, '7-3', 220, 'Right'),
+('Oso', 'Ighodaro', 22, 0, '6-11', 235, 'Right');
