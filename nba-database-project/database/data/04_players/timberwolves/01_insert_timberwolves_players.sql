10b8499840916f209968c38641b6b624b889f819
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 15:57:53 +0600

    Add Timberwolves players

---
+++ nba-database-project/database/data/04_players/timberwolves/01_insert_timberwolves_players.sql
@@ -0,0 +1,18 @@
+-- Insert Minnesota Timberwolves players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Anthony', 'Edwards', 18, 4, '6-4', 225, 'Right'),
+('Rudy', 'Gobert', 18, 11, '7-1', 258, 'Right'),
+('Julius', 'Randle', 18, 10, '6-9', 250, 'Left'),
+('Jaden', 'McDaniels', 18, 4, '6-9', 185, 'Right'),
+('Naz', 'Reid', 18, 5, '6-9', 264, 'Right'),
+('Donte', 'DiVincenzo', 18, 6, '6-4', 203, 'Right'),
+('Mike', 'Conley', 18, 17, '6-1', 175, 'Left'),
+('Rob', 'Dillingham', 18, 0, '6-2', 175, 'Right'),
+('Nickeil', 'Alexander-Walker', 18, 5, '6-5', 205, 'Right'),
+('Terrence ', 'Shannon Jr.', 18, 0, '6-6', 215, 'Left'),
+('Luka', 'Garza', 18, 3, '6-10', 243, 'Right'),
+('Joe', 'Ingles', 18, 10, '6-8', 220, 'Left'),
+('Josh', 'Minott', 18, 2, '6-8', 205, 'Right'),
+('Leonard', 'Miller', 18, 1, '6-10', 220, 'Left'),
+('PJ', 'Dozier', 18, 1, '6-6', 212, 'Right');
