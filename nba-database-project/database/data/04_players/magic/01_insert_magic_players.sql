cf3ae76a0e8d4f180ead7fddbaa315abb5269cba
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 21:41:51 +0600

    Add Magic players

---
+++ nba-database-project/database/data/04_players/magic/01_insert_magic_players.sql
@@ -0,0 +1,18 @@
+-- Insert Orlando Magic players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Jonathan', 'Isaac', 13, 5, '6-10', 230, 'Right'),
+('Kentavious', 'Caldwell-Pope', 13, 11, '6-5', 204, 'Right'),
+('Cole', 'Anthony', 13, 4, '6-2', 185, 'Right'),
+('Paolo', 'Banchero', 13, 2, '6-10', 250, 'Right'),
+('Wendell', 'Carter Jr.', 13, 6, '6-10', 270, 'Right'),
+('Moritz', 'Wagner', 13, 6, '6-11', 245, 'Right'),
+('Jalen', 'Suggs', 13, 3, '6-5', 205, 'Right'),
+('Goga', 'Bitadze', 13, 5, '6-11', 250, 'Right'),
+('Anthony', 'Black', 13, 1, '6-7', 200, 'Right'),
+('Gary', 'Harris', 13, 10, '6-4', 210, 'Right'),
+('Franz', 'Wagner', 13, 3, '6-10', 220, 'Right'),
+('Jett', 'Howard', 13, 1, '6-8', 215, 'Right'),
+('Tristan', 'Da Silva', 13, 0, '6-8', 217, 'Right'),
+('Cory', 'Joseph', 13, 13, '6-2', 200, 'Right'),
+('Caleb', 'Houstan', 13, 2, '6-8', 205, 'Right');
