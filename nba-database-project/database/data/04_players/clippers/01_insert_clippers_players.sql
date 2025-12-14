f0fe3fe254fd5205ee4ae0408eea5afa5a5047be
Choppy314 <umarbaevaz314@gmail.com>
2025-12-14 13:34:41 +0600

    Add Clippers players

---
+++ nba-database-project/database/data/04_players/clippers/01_insert_clippers_players.sql
@@ -0,0 +1,18 @@
+-- Insert Los Angeles Clippers players for 2024-25 season
+
+INSERT INTO players (first_name, last_name, team_id, experience, height, weight_pounds, shoots) VALUES
+('Kawhi', 'Leonard', 24, 12, '6-6', 225, 'Right'),
+('James', 'Harden', 24, 15, '6-5', 220, 'Left'),
+('Norman', 'Powell', 24, 9, '6-3', 215, 'Right'),
+('Bogdan', 'Bogdanović', 24, 7, '6-5', 225, 'Right'),
+('Ivica', 'Zubac', 24, 8, '7-0', 240, 'Right'),
+('Derrick', 'Jones Jr.', 24, 8, '6-6', 210, 'Left'),
+('Kris ', 'Dunn', 24, 8, '6-3', 205, 'Right'),
+('Drew', 'Eubanks', 24, 6, '6-10', 245, 'Right'),
+('Nicolas', 'Batum', 24, 16, '6-7', 230, 'Right'),
+('Amir', 'Coffey', 24, 5, '6-7', 210, 'Left'),
+('MarJon', 'Beauchamp', 24, 2, '6-7', 199, 'Right'),
+('Kobe', 'Brown', 24, 1, '6-7', 250, 'Right'),
+('Patty', 'Mills', 24, 15, '6-2', 180, 'Right'),
+('Cam', 'Christie', 24, 0, '6-5', 190, 'Right'),
+('Jordan', 'Miller', 24, 1, '6-5', 194, 'Left');
